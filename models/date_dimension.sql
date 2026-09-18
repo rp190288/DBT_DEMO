eecb0dacb54130963827ee0318558dc3d04c70cf
rp190288@gmail.com <rp190288@gmail.com>
2026-09-18 05:18:28 +0000

    daily_weather model created

---
+++ models/date_dimension.sql
@@ -0,0 +1,14 @@
+WITH CTE AS (
+select
+TO_TIMESTAMP(STARTED_AT) as STARTED_AT,
+DATE(TO_TIMESTAMP(STARTED_AT)) as DATE_STARTED_AT,
+HOUR(TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
+{{day_type('STARTED_AT')}} AS DAY_TYPE,
+{{get_season('STARTED_AT')}} as STATION_OF_YEAR
+from
+{{ source('demo', 'bike') }}
+
+)
+
+select * 
+from CTE
